71617b57f66959631d0cabf9156c5574c904fe1a
cuter <cuter9@msn.com>
2024-06-02 23:44:32 +0800

    add the version for ubuntu 20.04 and tensorflow V2

--- SSD_Work_Space/test_load_op.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-from tf_onnx import load_customer_op
-import os
-
-ol_path = os.path.join(os.environ['HOME'], 'Model_Conversion/tensorflow_trt_op/python3/ops/set/')
-a = load_customer_op(ol_path)
-# a = load_customer_op('tensorflow_trt_op/python3/ops/set/')
-# a = tf.load_op_library("/home/cuterbot/Model_Conversion/tensorflow_trt_op/python3/ops/set/_concat_trt_ops.so")
-dir(a)
